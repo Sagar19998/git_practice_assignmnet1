f19c06103b19262126fe95fb0cd7d2d21fb73728
jayeshdeshmukh18 <deshmukhjayesh507@gmail.com>
2022-09-20 20:02:17 +0530

    prime prime

--- prime1.js
+++ prime1.js
@@ -1,11 +1,13 @@
 let count=0;
 num=13;
-for(let i=0;i<=num;i--){
-	if(num%i){
-		count;
+for(let i=0;i<=num;i++){
+	if(num%i==0){
+		count++;
 	}
 }
 if(count==0){
 	console.log("prime");
 }
-else(" ")
+else{
+	console.log("Not prime")
+}
